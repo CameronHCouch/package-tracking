02467224779e638b833347d6bae8b6097428bec9
CameronHCouch <cameronhcouch@gmail.com>
2019-02-27 16:35:29 -0500

    Add demo user button to session form

--- app/models/delivery_status.rb
+++ app/models/delivery_status.rb
@@ -15,16 +15,16 @@ class DeliveryStatus < ApplicationRecord
   validates_inclusion_of :delivered?, { in: [true, false] }
   validates :timeline, inclusion: { in: ['normal','not normal', 'very late'] }, presence: true
 
-  after_initialize :update_normal_time 
-  before_update :update_timeline, :set_date_delivered
+  before_update :update_timeline, :set_date_delivered, :update_avg_time 
 
   has_one :order
   has_one :vendor, through: :order, source: :vendor
   has_one :user, through: :order, source: :user
 
-# TO DO: figure out when callback should run
-# TO DO: find out how to dynamically change normal time
+  @avg_time = nil
+  @order_count = 0
 
+# TO DO: figure out when callbacks should run to continously update normal_time
 
   def shipping_time_in_days
     (self.date_delivered - self.created_at) / 60 / 60 / 24
@@ -40,16 +40,19 @@ class DeliveryStatus < ApplicationRecord
     self.date_delivered = Time.now if self.delivered?
   end
 
-  def update_normal_time
+  # if there is no normal time, normal time is amnt it took to ship first item
+  # otherwise, add this shipping time to the running average
+  def update_avg_time
     if self.delivered? && self.date_delivered
-      if self.normal_time.nil?
-        self.update!(normal_time: self.shipping_time_in_days)
+      @order_count += 1
+      if @avg_time.nil?
+        @avg_time = self.shipping_time_in_days
       else
-        self.update!(normal_time: self.normal_time + ((self.shipping_time_in_days - self.normal_time) / (Order.all.count + 1))) 
+        @avg_time = @avg_time + ((self.shipping_time_in_days - @avg_time) / @order_count ) 
       end
     end
+    debugger
   end
-
 
   def update_timeline
     if self.normal_time && !self.delivered?
@@ -71,5 +74,3 @@ end
 # how to calculate normal time?
 # based specificially on vendor shipping to this specific location
 # right now, delivery status only knows a few things
-
-# do I need to convert address column on order table into location_id and have location model?
